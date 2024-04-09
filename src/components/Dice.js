12486146c30b9860fb417a6127b30ea662b25df1
TimDzen <140600844+TimDzen@users.noreply.github.com>
2024-04-09 16:04:37 +0300

    Update Dice.js

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Dice.css";
 
 const Dice = ({ diceValue }) => {
-  return <img src={`images/dice${diceValue}.png`} alt={`Dice ${diceValue}`} />;
+  return <img src={`images/dice${diceValue}.png`}  />;
 };
 
 export default Dice;
