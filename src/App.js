647f323ba3ba34da937788c20b601b6ba918f955
TimDzen <arktimur031@gmail.com>
2024-04-09 16:01:22 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Dice from './components/Dice';
 
-function App() {
+
+const App = () => {
+  const [diceValue, setDiceValue] = useState(1);
+
+  const rollDice = () => {
+    const newValue = Math.floor(Math.random() * 6) + 1;
+    setDiceValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Игра в Кубик</h1>
+      <Dice diceValue={diceValue} />
+      <button onClick={rollDice}>Тык!</button>
     </div>
   );
-}
+};
 
 export default App;
